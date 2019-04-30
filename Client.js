8c30ec9ffe72ae81cca4e1324dc0bda44c9a436d
Arjunamrith <amrith@tydy.it>
2019-04-30 17:22:35 +0530

    'implementing Node.JS socket server with ws'
    Co-authored-by: Amrithnath <arjun.amrith@gmail.com>

--- Client.js
+++ Client.js
@@ -1,1 +1,14 @@
-console.log('WebSocket client script will run here.');
+const url='ws://localhost:8080'
+const connection = new WebSocket(url)
+
+connection.onopen=()=>{
+    connection.send("hello server")
+}
+
+connection.onerror=error=>{
+    console.log(error)
+}
+
+connection.onmessage=e=>{
+    console.log(e.data)
+}
--- Server.js
+++ Server.js
@@ -1,8 +1,11 @@
-const http = require('http');
-const static = require('node-static');
-const file = new static.Server('./');
-const server = http.createServer((req, res) => {
-  req.addListener('end', () => file.serve(req, res)).resume();
-});
-const port = 3210;
-server.listen(port, () => console.log(`Server running at http://localhost:${port}`));
+const WebSocket=require('ws')
+port=8080
+const wss=new WebSocket.Server({port:port})
+console.log(`Server running on ${port}`)
+
+wss.on('connection',ws=>{
+    ws.on('message',message=>{
+        console.log(`received message=>${message}`)
+    })
+    ws.send('hello client')
+})
